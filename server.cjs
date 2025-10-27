7afea443b86f77b66e1824bf589567a4857efbca
raj-kariya <rajkariya2003@gmail.com>
2025-10-27 23:45:37 +0530

    Initial commit

---
+++ server.cjs
@@ -0,0 +1,31 @@
+// import express from 'express';
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuidV4} = require('uuid');
+const path = require('path')
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+
+app.get('/', (req,res) =>{
+    res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req,res)=>{
+    res.render('room', {roomId: req.params.room})
+})
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+    // socket.on('disconnect', () => {
+    //   socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    // })
+  })
+})
+server.listen(3000);
+
